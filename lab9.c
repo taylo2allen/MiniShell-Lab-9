14884efe05f4a536e54ec3479af515c361867723
taylo2allen <taylo2allen@gmail.com>
2018-11-02 09:47:19 -0700

    Refactoring.

--- lab9.c
+++ lab9.c
@@ -1,4 +1,4 @@
-/* Author(s): Jorge Tadeo
+/* Author(s): Jorge Tadeo & Taylor Allen
  *
  * This is lab9.c the csc60mshell
  * This program serves as a skeleton for doing labs 9, 10, 11.
@@ -24,7 +24,7 @@
 /* function prototypes */
 int parseline(char *cmdline, char **argv);
 
-//The two function prototypes below will be needed in lab10. 
+//The two function prototypes below will be needed in lab10.
 //Leave them here to be used later.
 /* void process_input(int argc, char **argv); */
 /* void handle_redir(int count, char *argv[]); */
@@ -43,56 +43,55 @@ int main(void)
 
     /* Loop forever to wait and process commands */
     while (TRUE) {
-	/* Print your shell name: csc60mshell (m for mini shell) */
-	printf("csc60mshell > ");
+      /* Print your shell name: csc60mshell (m for mini shell) */
+      printf("csc60mshell > ");
 
-	/* Read the command line */
-	fgets(cmdline, MAXLINE, stdin);
+      /* Read the command line */
+      fgets(cmdline, MAXLINE, stdin);
 
-	/* Call parseline to build argc/argv */
-    argc = parseline(cmdline, argv); 
+      /* Call parseline to build argc/argv */
+      argc = parseline(cmdline, argv);
 
-    /* Print out argc and argv list */
-    for(loop = 0; loop < argc ; loop++){
-            
-            printf("Argv %i = %s\n",loop,argv[loop]);
-             
+        /* Print out argc and argv list */
+        for(loop = 0; loop < argc ; loop++){
+          printf("Argv %i = %s\n",loop,argv[loop]);
+
            //  }
-	/* If user hits enter key without a command, continue to loop */
-	/* again at the beginning */
-	/*  Hint: if argc is zero, no command declared */
-	/*  Hint: look up for the keyword "continue" in C */
+  /* If user hits enter key without a command, continue to loop */
+  /* again at the beginning */
+  /*  Hint: if argc is zero, no command declared */
+  /*  Hint: look up for the keyword "continue" in C */
 
-	/* Handle build-in command: exit, pwd, or cd  */
-	/* Put the rest of your code here */
+  /* Handle build-in command: exit, pwd, or cd  */
+  /* Put the rest of your code here */
 
- 
-//.......................IGNORE........................	
+
+//.......................IGNORE........................
 //	/* Else, fork off a process */
 //      else {
-//	    pid = fork();
+//      pid = fork();
 //          switch(pid)
-//    	    {
-//	    	case -1:
-//		    perror("Shell Program fork error");
-//	            exit(EXIT_FAILURE);
-//	   	case 0:
-//		    /* I am child process. I will execute the command, */
-//		    /* and call: execvp */
-//		    process_input(argc, argv);
-//		    break;
-//	   	default:
-//		    /* I am parent process */
-//		    if (wait(&status) == -1)
-//		    	perror("Parent Process error");
-//		    else
-//		   	printf("Child returned status: %d\n",status);
-//		    break;
-//	    } 	/* end of the switch */
+//          {
+//        case -1:
+//        perror("Shell Program fork error");
+//              exit(EXIT_FAILURE);
+//      case 0:
+//        /* I am child process. I will execute the command, */
+//        /* and call: execvp */
+//        process_input(argc, argv);
+//        break;
+//      default:
+//        /* I am parent process */
+//        if (wait(&status) == -1)
+//          perror("Parent Process error");
+//        else
+//        printf("Child returned status: %d\n",status);
+//        break;
+//      }   /* end of the switch */
 //...end of the IGNORE above.........................
-	}	/* end of the if-else-if */
-    }		/* end of the while */
-} 		/* end of main */
+    }	/* end of the if-else-if */
+  }		/* end of the while */
+}     /* end of main */
 
 /* ----------------------------------------------------------------- */
 /*                  parseline                                        */
@@ -103,13 +102,13 @@ int parseline(char *cmdline, char **argv)
 {
     int count = 0;
     char *separator = " \n\t"; /* Includes space, Enter, Tab */
- 
+
     /* strtok searches for the characters listed in separator */
     argv[count] = strtok(cmdline, separator);
 
-    while ((argv[count] != NULL) && (count+1 < MAXARGS)) 
-    	argv[++count] = strtok((char *) 0, separator);
-     		
+    while ((argv[count] != NULL) && (count+1 < MAXARGS))
+      argv[++count] = strtok((char *) 0, separator);
+
     return count;  //This becomes "argc" back in main.
 }
 /* ----------------------------------------------------------------- */
@@ -127,7 +126,7 @@ int parseline(char *cmdline, char **argv)
     /*    fprintf(stderr, "Error on the exec call\n");              */
     /*    _exit(EXIT_FAILURE);                                      */
     /* }                                                            */
- 
+
 // }
 /* ----------------------------------------------------------------- */
 //void handle_redir(int count, char *argv[])
